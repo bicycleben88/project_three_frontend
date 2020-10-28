a8726dcac020002afab3c477bf8bfb07f715071b
Carlo Berardelli <owner@Carlos-MBP.fios-router.home>
2020-10-27 21:27:55 -0400

    added spacing in intro

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -4,7 +4,10 @@ function Intro() {
 
 return(
     <div>
-        <h3> Welcome to Cocktail Hour! Your go destination if your parched and looking to party. Feel free to <a href="#"> search for a drink</a> below, or hit <a href="#">"Screw It!"</a> To have a random drink populate for you. Additionally, you can <a href="#">signup/login</a> to create your own drink and save it for future reference.</h3>
+        <h3> 
+            Welcome to Cocktail Hour! <br/> 
+            Your go destination if your parched and looking to party. <br/> Feel free to <a href="#"> search for a drink</a> below, or hit <a href="#">"Screw It!"</a> To have a random drink populate for you. <br/>
+            Additionally, you can <a href="#">signup/login</a> to create your own drink and save it for future reference.</h3>
     </div>
     )
 }
