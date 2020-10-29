5371a3d06d5807117919f1a55b9ef88fac138d8a
Carlo Berardelli <owner@Carlos-MBP.fios-router.home>
2020-10-28 21:33:13 -0400

    pinned nav top top of screen

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import Nav from 'react-bootstrap/Nav'
 
 function Navigation(){
     return(
-        <Navbar bg="dark" variant="dark">
+        <Navbar id="stucktop" bg="dark" variant="dark">
             <Navbar.Brand href="#home">Cocktail Hour</Navbar.Brand>
             <Nav className="mr-auto">
             <Nav.Link href="#home">Home</Nav.Link>
