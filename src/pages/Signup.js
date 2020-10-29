b7102276e040010473386a5c9130f65606d1792f
Benjamin Higginbotham <benridesbikes@Benjamins-MacBook-Air.local>
2020-10-29 17:12:11 -0400

    css styles to signup page

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,8 +1,36 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Jumbotron from 'react-bootstrap/Jumbotron'
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form'
+import InputGroup from 'react-bootstrap/InputGroup'
 
 const Signup = (props) => {
     return(
-        <h1>Sign Up</h1>
+        <>
+        <Jumbotron>
+            <Container>
+                <h1>Sign Up</h1>
+            </Container>
+        </Jumbotron>
+        <InputGroup className="txtarea" >
+            <Form.Control
+            type="text"
+            placeholder="Create Username" 
+            style={{width: '155px'}}>
+            </Form.Control>
+            <Form.Control
+            type="password"
+            placeholder="Create Password" 
+            style={{width: '155px'}}>
+            </Form.Control>
+            <Form.Control
+            type="submit"
+            value="Sign Up" 
+            style={{width: '55px'}}>
+            </Form.Control>
+        </InputGroup>
+        </>
     )
 }
 
