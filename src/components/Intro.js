2139b1b908c2f35aac9d8d0ea7f9bc25d34008c3
Benjamin Higginbotham <benridesbikes@Benjamins-MacBook-Air.local>
2020-10-29 22:01:46 -0400

    add link

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -11,7 +11,7 @@ return(
         <h5> 
             Your go to destination if you're parched and looking to party. <br/> 
             Feel free to <a href="#"> search for a drink</a> below, or hit <a href="#">"Screw It!"</a> to have a random drink populate for you. <br/>
-            Additionally, you can <a href="#">signup/login</a> to create your own drink and save it for future reference.
+            Additionally, you can <a href="/signup">signup/login</a> to create your own drink and save it for future reference.
         </h5>
         </Container>
     </Jumbotron>
