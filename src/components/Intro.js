ccce92494a818b0574c31955b0a98e9415c742e8
Carlo Berardelli <owner@Carlos-MBP.fios-router.home>
2020-10-30 02:12:23 -0400

    added styling to landing page

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -8,7 +8,7 @@ return(
     <Jumbotron>
         <Container>
         <h1>Welcome to Cocktail Hour!</h1>
-        <h5> 
+        <h5 className="introtext"> 
             Your go to destination if you're parched and looking to party. <br/> 
             Feel free to <a href="#"> search for a drink</a> below, or hit <a href="#">"Screw It!"</a> to have a random drink populate for you. <br/>
             Additionally, you can <a href="/signup">signup/login</a> to create your own drink and save it for future reference.
